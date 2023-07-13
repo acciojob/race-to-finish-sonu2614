c1a6ee3bb743fab6ea1ec214e94c391dca041571
Sonu Kumar <60311092+sonu2614@users.noreply.github.com>
2023-07-13 15:23:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -29,7 +29,8 @@ let e = new Promise(resolve => {
 	},5000)
 })
 
-let x=Promise.any([a,b,c,d,e])
+let x=Promise.any([a, b, c, d, e])
+window.promises = [a, b, c, d, e];
 x.then((data) => {
 	const element=document.getElementById("output");
 	element.innerText=data;
